47834c6173415f831d02b876e9b15b8a26a5be30
Jose Meurer <josewmeurer@gmail.com>
2026-03-29 20:22:49 -0300

    Criação do gitignore, criação do shared.Integration pra arquivos compartilhados entre microservicos e classe entidade para o kafka

---
+++ src/Shared.Integration/Events/OrderPlacedEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Integration.Events;
+
+/// <summary>
+/// Evento disparado quando um cliente finaliza o checkout e o pedido é salvo no banco com sucesso.
+/// </summary>
+public class OrderPlacedEvent
+{
+    public Guid OrderId { get; init; }
+    public Guid CustomerId { get; init; }
+    public string? CustomerEmail { get; init; }
+    public decimal TotalAmount { get; init; }
+    public DateTime CreatedAt { get; init; }
+}

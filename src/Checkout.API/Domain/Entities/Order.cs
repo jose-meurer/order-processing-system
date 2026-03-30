e37a9f1cb1d97ebb5e0bdbe297fef48a9f8ea255
Jose Meurer <josewmeurer@gmail.com>
2026-03-29 21:22:51 -0300

    Criação do projeto Checkout.Api, enum OrderStatus e classe Order

---
+++ src/Checkout.API/Domain/Entities/Order.cs
@@ -0,0 +1,31 @@
+﻿using Checkout.API.Domain.Enums;
+
+namespace Checkout.API.Domain.Entities;
+
+public class Order
+{
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public string? CustomerEmail { get; private set; }
+    public decimal TotalAmount { get; private set; }
+    public OrderStatus Status { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    private Order() { }
+    
+    public Order(Guid customerId, string customerEmail, decimal totalAmount)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        CustomerEmail = customerEmail;
+        TotalAmount = totalAmount;
+        Status = OrderStatus.Pending;
+        CreatedAt = DateTime.UtcNow;
+    }
+    
+    public void MarkAsValidated() => Status = OrderStatus.Validated;
+    public void Reject() =>  Status = OrderStatus.Reject;
+    public void MarkAsPaid() => Status = OrderStatus.Paid;
+    public void FailPayment() => Status = OrderStatus.PaymentFailed;
+    public void Cancel() => Status = OrderStatus.Cancelled;
+}
---
+++ src/Checkout.API/Domain/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Checkout.API.Domain.Enums;
+
+public enum OrderStatus
+{
+    Pending = 1,
+    Validated = 2,
+    Reject = 3,
+    Paid = 4,
+    PaymentFailed = 5,
+    Cancelled = 6
+}
